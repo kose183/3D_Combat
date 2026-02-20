95906321907b0b71784fa799aa9bf5ac36cb0dbb
kose183 <kose183@naver.com>
2026-02-20 19:10:46 +0900

    p2
    p2

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Targeter : MonoBehaviour
 {
+    public List<Target> targets = new List<Target>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Target target = other.GetComponent<Target>();
+
+        if (target == null) return;
+
+        targets.Add(target);
+
+
+        //targets.Add(other.GetComponent<Target>());
+        //Debug.Log(other.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        targets.Remove(other.GetComponent<Target>());
+        Debug.Log(other.name);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
